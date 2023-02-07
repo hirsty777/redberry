c155a61a3c7189f5606afe324160dbb6189e55a6
hirsty777 <777hirsty@gmail.com>
2023-02-07 22:41:26 +0400

    experince page live update of input on page,add more experience button functions

--- js/experience.js
+++ js/experience.js
@@ -1,7 +1,10 @@
-
-
-
-
+//variables /////////////////////////////////////////////////
+//experience infromation page, inputs 
+const positions = document.querySelectorAll('.position-value');
+const employer = document.getElementById('employer');
+const starttime = document.getElementById('starttime');
+const endtime = document.getElementById('endtime');
+const experienceInfo = document.getElementById('experience-info');
 //personal infromation page, display inputs       
 const displayFirstname = document.getElementById('display-firstname');
 const displayLastname = document.getElementById('display-lastname');
@@ -14,9 +17,10 @@ const displayUploadedImage =document.getElementById('display-image');
 const addMoreExp = document.getElementById('add-more-experience');
 const experiencePrevBtn = document.getElementById('experience-prev-btn');
 
+let numb = 0;
 
 
-//display input values of personalinformation page  
+//display input values of personalinformation page  (previos page values)
 displayFirstname.textContent = localStorage.getItem('firstname');
 displayLastname.textContent = localStorage.getItem('lastname');
 if(localStorage.getItem('perosnalImg')){
@@ -29,10 +33,112 @@ displayNumber.textContent = localStorage.getItem('phone');
 
 
 
-const test = document.getElementById('test-div');
-const test2= document.getElementById('added-experience');
+
+//=>experience infromation page, display input values and verify 
+//position
+function positionInput(value, index){
+    const displayExp = document.querySelectorAll('.display-position');
+    displayExp[index].textContent = value
+}
 
 
+//employer
+function employerInput(value, index){
+    const displayEmployer = document.querySelectorAll('.display-employer');
+    displayEmployer[index].textContent = value
+}
+
+//start date 
+function startDateInput(value, index){
+    const displayStartTime = document.querySelectorAll('.display-start-date');
+    displayStartTime[index].textContent = value
+}
+
+//end date 
+function endDateInput(value, index){
+    const displayEndTime = document.querySelectorAll('.display-end-date');
+    displayEndTime[index].textContent = '- '+ value;
+}
+
+//experience info 
+function experienceInfInput(value, index){
+    const displayExperienceInf = document.querySelectorAll('.displaye-experince-info');
+    displayExperienceInf[index].textContent = value;
+}
+
+
+
+
+
+
+
+
+
+
+
+//add more experience options on click 
+const addMoreEepBox = document.getElementById('add-more-experience-box');
+const addMoreDisplayBox = document.getElementById('experience-live-display-added');
+addMoreExp.addEventListener('click',()=>{
+    numb += 1;
+
+    let div = document.createElement('div');
+    div.className = 'new-experience-div';
+    div.innerHTML=`
+<div class="position-box input-box-style1">
+    <label for="position" id="position-label" class="font-w5-s16">თანამდებობა</label>
+    <input type="text" id="position" class="font-w4-s16 input-style1" onkeyup="positionInput(value,${numb})" placeholder="დეველოპერი, დიზაინერი, ა.შ.">
+    <p class="font-w3-s14">მინიმუმ 2 სიმბოლო</p>
+</div> 
+<div class="employer-box input-box-style1">
+    <label for="employer" id="employer-label" class="font-w5-s16">დამსაქმებელი</label>
+    <input type="text" id="employer" class="font-w4-s16 input-style1" onkeyup="employerInput(value,${numb})" placeholder="დამსაქმებელი">
+    <p class="font-w3-s14">მინიმუმ 2 სიმბოლო</p>
+</div>
+<!--dates of work experience-->
+<div class="inputs-wrap work-date-box">
+    <div class="starttime-box input-box-style2">
+        <label for="starttime" id="starttime-label" class="font-w5-s16 ">დაწყების რიცხვი</label>
+        <input type="date" id="starttime" class="font-w4-s16 input-style1" onchange="startDateInput(value,${numb})">
+    </div>
+    <div class="endtime-box input-box-style2">
+        <label for="endtime" id="endtime-label" class="font-w5-s16 ">დამთავრების რიცხვი</label>
+        <input type="date" id="endtime" class="font-w4-s16 input-style1" onchange="endDateInput(value,${numb})">
+    </div>
+</div> 
+<!--description textarea for wor experience-->
+<div class="experience-info-box">
+    <span class="font-w5-s16">აღწერა</span>
+    <textarea name="experience info" id="experience-info" class="font-w4-s16" onkeyup="experienceInfInput(value,${numb})"
+    placeholder="როლი თანამდებობაზე და ზოგადი აღწერა"></textarea>
+</div>
+<hr class="end-line-experience">
+`;
+    addMoreEepBox.appendChild(div);
+
+    //===========================================
+    let displayDiv = document.createElement('div');
+    displayDiv.className = 'experience-live-display';
+    displayDiv.innerHTML = `
+<div class="experience-live-display">
+    <div class="display-position">
+        
+    </div>
+    <div class="display-employer">
+        
+    </div>
+    <div class="display-experience-date">
+        <span class="display-start-date"></span>
+        <span class="display-end-date"></span>
+    </div>
+    <div class="displaye-experince-info">
+
+    </div>
+ </div>
+ <div id="experience-live-display-added">
+</div>`;
+    addMoreDisplayBox.appendChild(displayDiv);
+});
 
 
 
